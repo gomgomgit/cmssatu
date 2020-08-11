5ab297a66c9787c3068f6b96b06a5de53ff11971
gomgomgit <thechocohead@gmail.com>
2020-08-11 22:44:57 +0700

    day3

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Model\Category;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -11,6 +13,14 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        Category::truncate();
+
+        $faker = Factory::create('id_ID');
+
+        for ($i = 0; $i < 50; $i++) {
+            Category::create([
+                'name' => $faker->word,
+            ]);
+        }
     }
 }
